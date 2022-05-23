291a2ae4a9d2b51628b7f15edfd9789711f1b832
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2022-05-23 14:42:01 +0300

    Added Player Controller and Follow camera

--- Maveric/Assets/Scrips/Player/PlayerController.cs
+++ Maveric/Assets/Scrips/Player/PlayerController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 rawInput;
+    void Update()
     {
+
+        Move();
+
+    }
+
+    void Move(){
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value){
+        rawInput = value.Get<Vector2>();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
