7941309fd5b96252170d84acca2e984ce23a2229
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2022-05-30 17:33:12 +0300

    adio

--- Maveric/Assets/Scrips/Player/CreatureData.cs
+++ Maveric/Assets/Scrips/Player/CreatureData.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 
 public class CreatureData : MonoBehaviour {
     public string creatureName;
+
+    public Transform player;
+
+    void Update () {
+        transform.position = new Vector3 (0, 0, 0);
+        transform.Rotate(0, 0, 0);
+    }
 }
